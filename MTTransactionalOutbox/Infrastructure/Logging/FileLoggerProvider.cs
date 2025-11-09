8908da02d8e70e53515c9a3f788098d091767fec
Joey Chömpff <joey.chompff@uitgekookt.nl>
2025-11-09 09:57:53 +0100

    Add FileLoggerProvider for custom file-based logging. Add log file as proof

---
+++ MTTransactionalOutbox/Infrastructure/Logging/FileLoggerProvider.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MTTransactionalOutbox.Infrastructure.Logging;
+
+public class FileLoggerProvider(string path) : ILoggerProvider
+{
+    private readonly object _lock = new();
+
+    public ILogger CreateLogger(string categoryName) => new FileLogger(path, _lock);
+
+    public void Dispose() { }
+
+    private class FileLogger(string path, object locker) : ILogger
+    {
+        public IDisposable? BeginScope<TState>(TState state) => null;
+
+        public bool IsEnabled(LogLevel logLevel) => true;
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId,
+            TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            if (!IsEnabled(logLevel)) return;
+
+            var message = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [{logLevel}] {formatter(state, exception)}";
+            lock (locker)
+            {
+                File.AppendAllText(path, message + Environment.NewLine);
+            }
+        }
+    }
+}
--- MTTransactionalOutbox/Program.cs
+++ MTTransactionalOutbox/Program.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using MTTransactionalOutbox.Infrastructure.Database;
+using MTTransactionalOutbox.Infrastructure.Logging;
 using MTTransactionalOutbox.Infrastructure.MassTransit;
 
 var builder = Host
     .CreateApplicationBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddProvider(new FileLoggerProvider("app.log"));
 
 builder.Configuration
     .SetBasePath(Directory.GetCurrentDirectory())
@@ -16,9 +22,6 @@ builder.Configuration
 builder
     .ConfigureDatabase()
     .ConfigureMassTransit();
-
-var debug = builder.Configuration.GetDebugView();
-Console.WriteLine(debug);
 
 var host = builder.Build();
 await host.RunAsync();
